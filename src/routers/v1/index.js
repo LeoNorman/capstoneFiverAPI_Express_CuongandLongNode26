63360c323a40b581ce6560ffd551b9d15af1c190
BanhmiPate1311 <huynhcuong@gmail.com>
2023-02-11 14:20:32 +0700

    update nguoidung loaicongviec

--- src/routers/v1/index.js
+++ src/routers/v1/index.js
@@ -15,7 +15,10 @@ const v1 = express.Router();
 
 // Định nghĩa các routers cho authorization
 v1.use("/auth", authRouter);
-// Định nghĩa các routers cho users
-v1.use("/users", authorization, userRouter);
+v1.use("/users", userRouter);
+// Định nghĩa các routers cho jobTyle
+v1.use("/jobtyles", jobTyleRouter);
+// Định nghĩa các routers cho hireJob
+v1.use("/hireJob", hireJobRouter);
 
 module.exports = v1;
